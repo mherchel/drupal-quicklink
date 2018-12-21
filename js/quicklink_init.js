3b6197c9957bc07e6c314feed52c9d47711e3cc2
Mike Herchel <mike@herchel.com>
2018-12-21 12:17:41 -0500

    A bit of refactoring of JS.

--- js/quicklink_init.js
+++ js/quicklink_init.js
@@ -5,11 +5,13 @@
     'attach': function attachQuicklink(context, settings) {
 
       function hydrateQuicklinkConfig() {
-        var quicklinkConfig = {};
+        settings.quicklink.quicklinkConfig = settings.quicklink.quicklinkConfig || {};
+
+        var quicklinkConfig = settings.quicklink.quicklinkConfig;
 
         quicklinkConfig.ignores = [];
 
-        // Loop through all the patters to ignore, and generate functions to populate quicklinkConfig.
+        // Loop through all the patters to ignore, and generate ES5 functions to populate quicklinkConfig.
         for (var i = 0; i < settings.quicklink.url_patterns_to_ignore.length; i++) {
           var pattern = settings.quicklink.url_patterns_to_ignore[i];
 
@@ -26,23 +28,24 @@
         // Ignore links that have a download attribute.
         quicklinkConfig.ignores.push((uri, elem) => elem.hasAttribute('download'));
 
-        quicklinkConfig.el = context;
-
         if (settings.quicklink.selector) {
-          quicklinkConfig.el = context.querySelector(drupalSettings.quicklink.selector);
+          quicklinkConfig.el = context.querySelector(settings.quicklink.selector);
         }
 
         if (settings.quicklink.allowed_domains) {
-          quicklinkConfig.origins = drupalSettings.quicklink.allowed_domains;
+          quicklinkConfig.origins = settings.quicklink.allowed_domains;
         }
-
-        return quicklinkConfig;
       }
 
-      var quicklinkConfig = quicklinkConfig || hydrateQuicklinkConfig();
+      if (!settings.quicklink.quicklinkConfig) {
+        hydrateQuicklinkConfig()
+      }
+
+      // Update element to context every time Drupal.behaviors is run.
+      settings.quicklink.quicklinkConfig.el = context;
 
       if (window.quicklink) {
-        quicklink(quicklinkConfig);
+        quicklink(settings.quicklink.quicklinkConfig);
       }
     },
   };
